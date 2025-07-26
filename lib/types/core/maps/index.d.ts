bc40e58421b76ca7f7077e3617b324e16566fe21
ACTEPUKC <makakakakala@gmail.com>
2025-07-26 23:15:08 +0300

    koth stats

--- lib/types/core/maps/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { TLogger, TMaps } from '../../types';
-export declare const initMaps: (mapsName: string, logger: TLogger) => Promise<TMaps>;
--- lib/types/plugins/types.d.ts
+++
@@ -1,7 +0,0 @@
-export type TPluginsState = {
-    discord?: TDiscord;
-};
-type TDiscord = {
-    sendMessage: (channelId: string, text: string) => void;
-};
-export {};
